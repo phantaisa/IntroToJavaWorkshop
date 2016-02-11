0ca8e848466f6f1fd420b1f40704ead03ae3bac4
Mrfuzzy <league@Mac-mini-2.local>
2016-02-10 17:33:19 -0800

    i programed a robot and made it make a square with some help of the people here

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -4,12 +4,17 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	public static void main(String[] args) {
-		
-		// START HERE
+	public static void main(String[] args) 
 
+	Robot Phanbot=new Robot();
+
+	for (int i = 0; i < 4; i++) 
 	
+	{Phanbot.penDown();
+		Phanbot.move(10);
+		Phanbot.turn(90);}
 		
 		
-	}
-}
+	
+	    
+
